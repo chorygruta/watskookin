32de4a12a230b862f04cec1154346ed41de63e68
Chory Gruta <chorygruta@gmail.com>
2017-08-21 16:34:18 -0700

    Commented out server_name = localhost

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ SECRET_KEY = 'thisisasecret'
 SQLALCHEMY_DATABASE_URI = 'postgresql://fctntwrmfooapf:8897d30c490ecfbd4182e5e8a60966b57f6cd0557ee2ed4c880463e9619189f4@ec2-54-163-237-25.compute-1.amazonaws.com:5432/d6nnmpc09qqr2n'
 CSRF_ENABLED = True
 USER_ENABLE_EMAIL = False
-SERVER_NAME = 'localhost:5000'
+#SERVER_NAME = 'localhost:5000'
 WHOOSH_BASE = 'whoosh'
